e9b318a464845b9f2a0a10b54ccbf39f749565b1
kumiori <a.leon.baldelli@gmail.com>
2024-10-16 14:52:27 +0200

    dipole runs, bad scalings

--- src/disclinations/test/test_parametric-dipole.py
+++ src/disclinations/test/test_parametric-dipole.py
@@ -24,7 +24,8 @@ from disclinations.solvers import SNESSolver
 from disclinations.utils import (Visualisation, memory_usage, monitor,
                                  table_timing_data, write_to_output, 
                                  homogeneous_dirichlet_bc_H20,
-                                 initialise_exact_solution_dipole)
+                                 initialise_exact_solution_dipole,
+                                 exact_energy_dipole)
 from disclinations.utils.la import compute_disclination_loads
 from dolfinx import fem
 from dolfinx.common import list_timings
@@ -53,12 +54,70 @@ from disclinations.utils import create_or_load_circle_mesh
 import importlib.resources as pkg_resources  # Python 3.7+ for accessing package files
 import copy
 
-def run_experiment(mesh, parameters, series, variant = "variational"):
+def compute_energy_errors(energy_terms, exact_energy_dipole):
+    computed_membrane_energy = energy_terms["membrane"]
+    error = np.abs(exact_energy_dipole - computed_membrane_energy)
+
+    print(f"Exact energy: {exact_energy_dipole:.3e}")
+    print(f"Computed energy: {computed_membrane_energy:.3e}")
+    print(f"Abs error: {error:.3%}")
+    print(f"Rel error: {error/exact_energy_dipole:.3%}")
+
+    return error, error / exact_energy_dipole
+
+def postprocess(state, model, mesh, params, exact_solution, prefix):
+    with dolfinx.common.Timer(f"~Postprocessing and Vis") as timer:
+        energy_components = {
+            "bending": model.energy(state)[1],
+            "membrane": model.energy(state)[2],
+            "coupling": model.energy(state)[3],
+            "external_work": -model.W_ext,
+        }
+
+        energy_terms = compute_energy_terms(energy_components, mesh.comm)
+        distance = np.linalg.norm(params['loading']['points'][0] - params['loading']['points'][1])
+        
+        exact_energy_dipole = params["model"]["E"] * params["model"]["thickness"]**3 \
+            * params["geometry"]["radius"]**2 / (8 * np.pi) *  distance**2 * \
+                (np.log(4+distance**2) - np.log(4 * distance))
+
+        print(yaml.dump(params["model"], default_flow_style=False))
+        
+        abs_error, rel_error = compute_energy_errors(
+            energy_terms, exact_energy_dipole
+        )
+
+        if exact_solution is not None:
+            _v_exact, _w_exact = exact_solution
+        else:
+            _v_exact, _w_exact = None, None
+            
+        extra_fields = [
+            {"field": _v_exact, "name": "v_exact"},
+            {"field": _w_exact, "name": "w_exact"},
+            {
+                "field": model.M(state["w"]),  # Tensor expression
+                "name": "M",
+                "components": "tensor",
+            },
+            {
+                "field": model.P(state["v"]),  # Tensor expression
+                "name": "P",
+                "components": "tensor",
+            },
+        ]
+        # write_to_output(prefix, q, extra_fields)
+        return abs_error, rel_error
+
+
+
+def run_experiment(mesh, parameters, experiment_dir, variant = "variational"):
     print("Running experiment of the series", series, " with thickness:", parameters["model"]["thickness"])
     
     parameters = calculate_rescaling_factors(parameters)
-
-    prefix = os.path.join(outdir, f"plate_fvk_parametric_dipole-{series}")
+    signature = hashlib.md5(str(parameters).encode('utf-8')).hexdigest()
+    
+    prefix = os.path.join(experiment_dir, signature)
     if comm.rank == 0:
         Path(prefix).mkdir(parents=True, exist_ok=True)
 
@@ -98,7 +157,7 @@ def run_experiment(mesh, parameters, series, variant = "variational"):
     q = dolfinx.fem.Function(Q)
     v, w = ufl.split(q)
     state = {"v": v, "w": w}
-    _W_ext = W_ext = Constant(mesh, np.array(0.0, dtype=PETSc.ScalarType)) * w * dx
+    _W_ext = Constant(mesh, np.array(0.0, dtype=PETSc.ScalarType)) * w * dx
     # Define the variational problem
 
     Q_v, Q_v_to_Q_dofs = Q.sub(AIRY).collapse()
@@ -159,20 +218,23 @@ def run_experiment(mesh, parameters, series, variant = "variational"):
 
     solver.solve()
     exact_solution = initialise_exact_solution_dipole(Q, params)
-    
-    pdb.set_trace()
-        
-    return np.random.rand(1)
+
+    abs_error, rel_error = postprocess(
+        state, model, mesh, params=params, exact_solution=exact_solution, prefix=prefix
+    )
+
+    return abs_error, rel_error
 
 if __name__ == "__main__":
     from disclinations.utils import table_timing_data, Visualisation
     _experimental_data = []
     outdir = "output"
-    prefix = os.path.join(outdir, "test_parametric")
+    prefix = os.path.join(outdir, "parametric_dipole_vs_thickness")
     
     with pkg_resources.path('disclinations.test', 'parameters.yml') as f:
         # parameters = yaml.load(f, Loader=yaml.FullLoader)
         parameters, _ = load_parameters(f)
+
         base_parameters = copy.deepcopy(parameters)
         # Remove thickness from the parameters, to compute the parametric series signature
         if "model" in base_parameters and "thickness" in base_parameters["model"]:
@@ -195,12 +257,23 @@ if __name__ == "__main__":
     
     with dolfinx.common.Timer(f"~Computation Experiment") as timer:
 
-        for i, thickness in enumerate(np.linspace(0.1, 1, num_runs)):
-            if changed := update_parameters(parameters, "thickness", thickness):
-               data = run_experiment(mesh, parameters, series)
+        for i, thickness in enumerate(np.linspace(.001, 1, num_runs)):
+            if changed := update_parameters(parameters, "thickness", float(thickness)):
+                signature = hashlib.md5(str(parameters).encode('utf-8')).hexdigest()
+                abs_error, rel_error = run_experiment(mesh, parameters, experiment_dir)
             else: 
+                abs_error, rel_error = None, None
                 raise ValueError("Failed to update parameters")
-            _experimental_data.append(data)
+            
+            _data = {
+                "thickness": thickness,
+                "signature": signature,
+                "relative_error": rel_error,
+                "absolute_error": abs_error,
+                "exact_energy": exact_energy_dipole(parameters)
+            }
+
+            _experimental_data.append(_data)
     
     experimental_data = pd.DataFrame(_experimental_data)
     
