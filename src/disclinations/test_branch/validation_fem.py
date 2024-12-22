Bug on adimensional.py solved (hessian norm squared is used now in place of laplacian**2), plot modified.

src/disclinations/test_branch/validation_fem.py
@@ -11,6 +11,7 @@ import importlib.resources as pkg_resources  # Python 3.7+ for accessing package
 import yaml
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter, MaxNLocator
 import pdb
 import os
 import pandas as pd
@@ -18,10 +19,6 @@ import argparse
 
 from disclinations.utils import load_parameters
 from disclinations.utils.viz import plot_scalar, plot_profile, plot_mesh
-
-# parser = argparse.ArgumentParser(description="Which comparison would you like to run?")
-# parser.add_argument("test type", help="Either disclination or transverse")
-# parser.add_argument("dimensional / adimensional model", help="Either disclination or transverse")
 
 SCRIPT_VAR = "validation_test1_adim"
 SCRIPT_BRN = "validation_test1_BR_adim"
@@ -56,12 +53,12 @@ V_w = script_var["V_w"]
 dofs_v = script_var["dofs_v"]
 dofs_w = script_var["dofs_w"]
 
-v_exact = script_var["v_exact"]
+v_exact = script_var["v_exact_adim"]
 v_var = script_var["vpp"]
 v_brn = script_brn["vpp"]
 v_car = script_car["vpp"]
 
-w_exact = script_var["w_exact"]
+w_exact = script_var["w_exact_adim"]
 w_var = script_var["wpp"]
 w_brn = script_brn["wpp"]
 w_car = script_car["wpp"]
@@ -185,34 +182,47 @@ xs = np.linspace(-parameters["geometry"]["radius"] + tol, parameters["geometry"]
 points = np.zeros((3, 101))
 points[0] = xs
 
-fig, axes = plt.subplots(1, 1, figsize=(24, 18))
+FIGWIDTH = 17
+FIGHEIGHT = 12
+
+fig, axes = plt.subplots(1, 1, figsize=(FIGWIDTH, FIGHEIGHT))
 
 _plt, data = plot_profile(w_var, points, None, subplot=(1, 1), lineproperties={"c": "b", "lw":7, "label": "VAR"}, fig=fig, subplotnumber=1)
 _plt, data = plot_profile(w_brn, points, None, subplot=(1, 1), lineproperties={"c": "r", "lw":7, "label": "BNRS17", "ls": ":"}, fig=fig, subplotnumber=1)
 _plt, data = plot_profile(w_car, points, None, subplot=(1, 1), lineproperties={"c": "g", "lw":7, "label": "CMN18", "ls": "--"}, fig=fig, subplotnumber=1)
 _plt, data = plot_profile(w_exact, points, None, subplot=(1, 1), lineproperties={"c": "k", "lw":7, "label": "Analytical solution", "ls": "--"}, fig=fig, subplotnumber=1)
 
-_plt.xlabel("x [m]", fontsize=35)
-_plt.ylabel("Transverse displacement [m]", fontsize=35)
+_plt.xlabel(r"$\xi_1$", fontsize=35)
+_plt.ylabel(r"$\tilde{w}$", fontsize=35)
 _plt.xticks(fontsize=35)
 _plt.yticks(fontsize=35)
 _plt.title(f"Comparison between FE models. Transverse displacement. {info_experiment}", size = 30)
-_plt.grid(True)
+#_plt.grid(True)
 _plt.legend(fontsize=30)
+ax = _plt.gca() # use scientific notation for y axis
+ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+ax.yaxis.get_offset_text().set_fontsize(30)
+ax.xaxis.set_major_locator(MaxNLocator(nbins=5))  # Adjust the number of bins to choose the number of ticks
 _plt.savefig(f"{OUTDIR}/{SCRIPT_VAR}-w-profiles.png")
 
-fig, axes = plt.subplots(1, 1, figsize=(24, 18))
+fig, axes = plt.subplots(1, 1, figsize=(FIGWIDTH, FIGHEIGHT))
 
 _plt, data = plot_profile(v_exact, points, None, subplot=(1, 1), lineproperties={"c": "k", "lw":7, "label": "Analytical solution", "ls": "--"}, fig=fig, subplotnumber=1)
 _plt, data = plot_profile(v_var, points, None, subplot=(1, 1), lineproperties={"c": "b", "lw":7, "label": "VAR"}, fig=fig, subplotnumber=1)
 _plt, data = plot_profile(v_brn, points, None, subplot=(1, 1), lineproperties={"c": "r", "lw":7, "label": "BNRS17", "ls": ":"}, fig=fig, subplotnumber=1)
 _plt, data = plot_profile(v_car, points, None, subplot=(1, 1), lineproperties={"c": "g", "lw":7, "label": "CMN18", "ls": "--"}, fig=fig, subplotnumber=1)
 
-_plt.xlabel("x [m]", fontsize=35)
-_plt.ylabel("Airy's function [Nm]", fontsize=35)
+_plt.xlabel(r"$\xi_1$", fontsize=35)
+_plt.ylabel(r"$\tilde{v}$", fontsize=35)
 _plt.xticks(fontsize=35)
 _plt.yticks(fontsize=35)
 _plt.title(f"Comparison between FE models. Airy's function. {info_experiment}", size = 30)
-_plt.grid(True)
+#_plt.grid(True)
 _plt.legend(fontsize=30)
+ax = _plt.gca() # use scientific notation for y axis
+ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+ax.yaxis.get_offset_text().set_fontsize(30)
+ax.xaxis.set_major_locator(MaxNLocator(nbins=5))  # Adjust the number of bins to choose the number of ticks
 _plt.savefig(f"{OUTDIR}/{SCRIPT_VAR}-v-profiles.png")

src/disclinations/test_branch/validation_test1_adim.py
@@ -126,15 +126,17 @@ Q = dolfinx.fem.functionspace(mesh, basix.ufl.mixed_element([X, X]))
 
 # DEFINE FUNCTIONS
 q = dolfinx.fem.Function(Q)
-v, w = ufl.split(q)
 q_exact = dolfinx.fem.Function(Q)
+q_exact_adim = dolfinx.fem.Function(Q)
+v, w = ufl.split(q)
 v_exact, w_exact = q_exact.split()
-v_dim = E*(thickness**3)*v
-w_dim = thickness*w
+v_exact_adim, w_exact_adim = q_exact_adim.split()
+#v_dim = E*(thickness**3)*v
+#w_dim = thickness*w
 f = dolfinx.fem.Function(Q.sub(TRANSVERSE).collapse()[0])
 state = {"v": v, "w": w}
 state_exact = {"v": v_exact, "w": w_exact}
-state_dimensional = {"v": v_dim, "w": w_dim}
+#state_dimensional = {"v": v_dim, "w": w_dim}
 
 # SET BOUDNARY CONDITIONS
 mesh.topology.create_connectivity(mesh.topology.dim - 1, mesh.topology.dim)
@@ -159,8 +161,14 @@ def v_exact_function(x):
 
 def w_exact_function(x): return np.sqrt(2*D/(E*thickness)) * (1 - x[0]**2 - x[1]**2)**2
 
+V_v, dofs_v = Q.sub(0).collapse()
+V_w, dofs_w = Q.sub(1).collapse()
+
 v_exact.interpolate(v_exact_function)
 w_exact.interpolate(w_exact_function)
+v_exact_adim.vector.array.real[dofs_v] = v_exact.vector.array.real[dofs_v] / (E*(thickness**3))
+w_exact_adim.vector.array.real[dofs_w] = w_exact.vector.array.real[dofs_w] / thickness
+
 exact_energy = model.energy(state_exact)[0]
 ex_bending_energy = exact_bending_energy(v_exact, w_exact)
 ex_membrane_energy = exact_membrane_energy(v_exact, w_exact)
@@ -261,12 +269,10 @@ fig = ax.get_figure()
 fig.savefig(f"{OUTDIR}/mesh.png")
 
 # Plot the profiles
-V_v, dofs_v = Q.sub(0).collapse()
-V_w, dofs_w = Q.sub(1).collapse()
 pyvista.OFF_SCREEN = True
 
-vpp.vector.array.real[dofs_v] = E*(thickness**3) * vpp.vector.array.real[dofs_v]
-wpp.vector.array.real[dofs_w] = thickness * wpp.vector.array.real[dofs_w]
+#vpp.vector.array.real[dofs_v] = E*(thickness**3) * vpp.vector.array.real[dofs_v]
+#wpp.vector.array.real[dofs_w] = thickness * wpp.vector.array.real[dofs_w]
 
 plotter = pyvista.Plotter(title="Displacement", window_size=[1200, 600], shape=(2, 2) )
 

src/disclinations/test_branch/validation_test2_CA_adim.py
@@ -277,9 +277,9 @@ wpp.name = "deflection"
 
 V_v, dofs_v = Q.sub(0).collapse()
 V_w, dofs_w = Q.sub(1).collapse()
-
-vpp.vector.array.real[dofs_v] = E*(thickness**3) * vpp.vector.array.real[dofs_v]
-wpp.vector.array.real[dofs_w] = thickness * wpp.vector.array.real[dofs_w]
+print("sup norm w_car: ", max(abs(wpp.vector.array.real[dofs_w])) )
+#vpp.vector.array.real[dofs_v] = E*(thickness**3) * vpp.vector.array.real[dofs_v]
+#wpp.vector.array.real[dofs_w] = thickness * wpp.vector.array.real[dofs_w]
 
 _pv_points = np.array([p[0] for p in disclinations])
 _pv_colours = np.array(-np.array(DISCLINATION_POWER_LIST))

src/disclinations/test_branch/validation_test2_adim.py
@@ -137,8 +137,10 @@ bcs = list(_bcs.values())
 # DEFINE FUNCTIONS
 q = dolfinx.fem.Function(Q)
 q_exact = dolfinx.fem.Function(Q)
+q_exact_adim = dolfinx.fem.Function(Q)
 v, w = ufl.split(q)
 v_exact, w_exact = q_exact.split()
+v_exact_adim, w_exact_adim = q_exact_adim.split()
 state = {"v": v, "w": w}
 
 # DISCLINATION DISTRIBUTION
@@ -162,8 +164,16 @@ def function_w_exact(x):
     _w = 0.0*_w
     return _w
 
+
+V_v, dofs_v = Q.sub(0).collapse()
+V_w, dofs_w = Q.sub(1).collapse()
+
+
 v_exact.interpolate(function_v_exact)
 w_exact.interpolate(function_w_exact)
+
+v_exact_adim.vector.array.real[dofs_v] = v_exact.vector.array.real[dofs_v] / (E*(thickness**3))
+w_exact_adim.vector.array.real[dofs_w] = w_exact.vector.array.real[dofs_w] / thickness
 
 def compute_exact_energy_dipole(v_exact):
     energy = 0.0
@@ -280,9 +290,9 @@ wpp.name = "deflection"
 
 V_v, dofs_v = Q.sub(0).collapse()
 V_w, dofs_w = Q.sub(1).collapse()
-
-vpp.vector.array.real[dofs_v] = E*(thickness**3) * vpp.vector.array.real[dofs_v]
-wpp.vector.array.real[dofs_w] = thickness * wpp.vector.array.real[dofs_w]
+print("sup norm w_var: ", max(abs(wpp.vector.array.real[dofs_w])) )
+#vpp.vector.array.real[dofs_v] = E*(thickness**3) * vpp.vector.array.real[dofs_v]
+#wpp.vector.array.real[dofs_w] = thickness * wpp.vector.array.real[dofs_w]
 
 _pv_points = np.array([p[0] for p in disclinations])
 _pv_colours = np.array(-np.array(DISCLINATION_POWER_LIST))